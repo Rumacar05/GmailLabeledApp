5bfc8ecbd812cd4bfb6b140cce9e6a33be2a28f1
Rumacar05 <Rumacar05@gmail.com>
2025-03-01 21:26:03 +0100

    Update user interface

--- src/main/java/com/ruma/gmaillabeledapp/controller/ManageEmailController.java
+++ src/main/java/com/ruma/gmaillabeledapp/controller/ManageEmailController.java
@@ -8,10 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 
 public class ManageEmailController {
 
@@ -19,14 +16,14 @@ public class ManageEmailController {
     private Button btnLoadEmails;
     @FXML
     private Button btnSetDone;
-
     @FXML
     private Button btnSetInProgress;
-
     @FXML
     private Button btnSetToBeDone;
     @FXML
     private Label lblEmail;
+    @FXML
+    private ProgressBar progressBar;
     @FXML
     private ListView<Email> lvEmails;
 
@@ -40,6 +37,7 @@ public class ManageEmailController {
 
     @FXML
     void onLoadEmailsClicked() {
+        progressBar.setVisible(true);
         disableButtons(true);
 
         Task<ObservableList<Email>> task = new Task<>() {
@@ -53,6 +51,7 @@ public class ManageEmailController {
             ObservableList<Email> emails = task.getValue();
             lvEmails.setItems(emails);
             disableButtons(false);
+            progressBar.setVisible(false);
         });
 
         task.setOnFailed(event -> {
@@ -61,6 +60,7 @@ public class ManageEmailController {
                     "No se han podido cargar los emails: " + ex.getMessage());
 
             disableButtons(false);
+            progressBar.setVisible(false);
         });
 
         new Thread(task).start();
@@ -82,15 +82,39 @@ public class ManageEmailController {
     }
 
     private void setLabelForEmail(String labelFolder) {
-        try {
-            Email email = lvEmails.getSelectionModel().getSelectedItem();
-            if (email != null) {
-                emailService.setFolderForEmail(email, labelFolder);
+        progressBar.setVisible(true);
+        disableButtons(true);
+
+        Email email = lvEmails.getSelectionModel().getSelectedItem();
+        if (email != null) {
+            Task<Void> task = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    emailService.setFolderForEmail(email, labelFolder);
+                    return null;
+                }
+            };
+
+            task.setOnSucceeded(event -> {
                 lvEmails.refresh();
-            }
-        } catch (Exception ex) {
-            AlertService.showAlert(Alert.AlertType.ERROR, "Error",
-                    "No se ha podido poner la etiqueta: " + ex.getMessage());
+                progressBar.setVisible(false);
+                disableButtons(false);
+            });
+
+
+            task.setOnFailed(event -> {
+                progressBar.setVisible(false);
+                disableButtons(false);
+
+                Throwable ex = task.getException();
+                AlertService.showAlert(Alert.AlertType.ERROR, "Error",
+                        "No se ha podido poner la etiqueta: " + ex.getMessage());
+            });
+
+            new Thread(task).start();
+        } else {
+            AlertService.showAlert(Alert.AlertType.WARNING, "Advertencia",
+                    "No se ha seleccionado ningún correo");
         }
     }
 
@@ -100,5 +124,4 @@ public class ManageEmailController {
         btnSetInProgress.setDisable(b);
         btnSetToBeDone.setDisable(b);
     }
-
 }
